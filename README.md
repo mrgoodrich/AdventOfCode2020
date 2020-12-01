074a54d4c2ad2fad7e7683138251c8be4194ede0
Matt Goodrich <mryangoodrich6@gmail.com>
2020-11-30 19:05:36 -0500

    2020 AoC here we go

---
+++ README.md
@@ -0,0 +1,2 @@
+# AdventOfCode2020
+Focusing on speed over quality and readability. adventofcode.com/2020
---
+++ Template/first.js
@@ -0,0 +1,27 @@
+// ----------- PACKAGES -----------------------
+const fs = require('fs');
+const {all, any, contains, enumerate, filter, iter, map, partition, permutations, range, reduce, reduce_, sorted, sum, toArray, zip, zip3} = require('iter-tools');
+const {max, min} = require('itertools');
+// const Map = require('collections/map');
+// const pycollections = require('pycollections');
+
+// ----------- INPUT SETUP --------------------
+const input = fs.readFileSync('inputs/input', 'utf8');
+
+// ----------- TESTING AND REGEX -----®---------
+const nonWhitespace = /\S+/g;
+const positiveOrNegativeNumber = /(-?)(\d+)/g;
+const numberAndWord = /(-?)(\d+) \S+/g;
+const entireLine = /^(\S*)(?:\n)/gm
+
+const regex = entireLine;
+
+// ----------- SOLUTION ------------------------
+let result;
+while((result = regex.exec(input)) !== null) {
+  const entireMatch = result[0]; // has new line char if entireLine
+  const firstGroup = result[1];
+  console.log(result[1]);
+}
+
+// ----------- GUESSES -------------------------
